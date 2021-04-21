502bf8a5278322d0ee76419374a10ee64af81423
Martin Penkov <mirander73@gmail.com>
2021-04-22 00:39:30 +0300

    Notebook back-end API initial commit

---
+++ NotebookBotAPI/NotebookBotAPI/Models/NotebookDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotebookBotAPI.Models
+{
+    public class NotebookDbContext : DbContext
+    {
+        public NotebookDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
