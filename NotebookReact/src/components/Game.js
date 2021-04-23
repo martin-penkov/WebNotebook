6f5fd44a1df7bb51ced22c1463e70432abd07ca0
Martin Penkov <mirander73@gmail.com>
2021-04-23 23:49:06 +0300

    .

---
+++ NotebookReact/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from './Board';
+ 
+class Game extends React.Component{
+    render(){
+        return (
+            <Board></Board>
+        )
+    }
+}
+
+export default Game
