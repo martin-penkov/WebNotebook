ba9b2606ec4505c1c657f8ed8b27548a213ddf6d
martin_penkov <mirander73@gmail.com>
2021-05-23 12:28:28 +0300

    main menu component created

--- NotebookReact/src/components/HomeWrapper.js
+++ NotebookReact/src/components/HomeWrapper.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import {authenticationService as auth} from '../services/auth'
+import LandingPage from './LandingPage'
+import Album from './test'
 
 export default class HomeWrapper extends React.Component{
     constructor(props){
@@ -8,10 +11,19 @@ export default class HomeWrapper extends React.Component{
     }
 
     checkUserLoginStatus(){
-
+        return auth.currentUserValue()
     }
 
     render(){
-        return <div></div>
+        if(this.checkUserLoginStatus()){
+            //jwt auth token found in browser local storage
+            return (
+            <div className='personDashboard'>
+                <Album/>
+            </div>
+            )
+        }
+        //show landing page with options for user login...
+        return <LandingPage/>
     }
 }
